6309751e3a2161759f52bf6c63b6a0578f852d0f
kartikgits <kartik.kapparwan1@gmail.com>
2018-03-18 00:12:44 +0530

    Added navigation drawer menu without nav drawer button and other styling

--- app/src/main/java/com/northwindlabs/kartikeya/recipezest/ExploreFragment.java
+++ app/src/main/java/com/northwindlabs/kartikeya/recipezest/ExploreFragment.java
@@ -1,5 +1,6 @@
 package com.northwindlabs.kartikeya.recipezest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
--- app/src/main/java/com/northwindlabs/kartikeya/recipezest/RecipeActivity.java
+++ app/src/main/java/com/northwindlabs/kartikeya/recipezest/RecipeActivity.java
@@ -1,16 +1,22 @@
 package com.northwindlabs.kartikeya.recipezest;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by Kartikeya on 2/14/2018.
  */
 
 public class RecipeActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -22,8 +28,34 @@ public class RecipeActivity extends AppCompatActivity {
         //Set the content of the activity to use activity_recipe.xml layout file
         setContentView(R.layout.activity_recipe);
 
+        //Navigation drawer
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        /**
+         * When an item is tapped, this code sets the selected item as checked,
+         * changing the list item's style to be highlighted because the list items
+         * are part of a checkable group
+         */
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+                        /**
+                         * Add code here to update the UI based on the item selected.
+                         * For example, swap UI fragments here
+                         */
+                        return true;
+                    }
+                }
+        );
+
         //Find the viewPager that will allow the user to swipe between fragments
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        ViewPager viewPager = findViewById(R.id.viewpager);
 
         //Create an adapter that knows which fragment is to be shown on each page
         RecipeFragmentPager adapter = new RecipeFragmentPager(getSupportFragmentManager(), RecipeActivity.this);
@@ -32,7 +64,7 @@ public class RecipeActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);
 
         //Give the tabLayout to the viewPager
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        TabLayout tabLayout = findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
     }
 }
