50f6fa4962fa3eebafc100d68d3eef01f0bce1b2
kartikgits <kartik.kapparwan1@gmail.com>
2018-05-29 15:07:15 +0530

    Make MyFavorites Activity work

--- app/src/main/java/com/northwindlabs/kartikeya/recipezest/MyFavoriteActivity.java
+++ app/src/main/java/com/northwindlabs/kartikeya/recipezest/MyFavoriteActivity.java
@@ -1,6 +1,9 @@
 package com.northwindlabs.kartikeya.recipezest;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -11,9 +14,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import static com.northwindlabs.kartikeya.recipezest.RZestContract.UserFavorites.COLUMN_FAVORITE_RECIPE_NAME;
+import static com.northwindlabs.kartikeya.recipezest.RZestContract.UserFavorites.TABLE_NAME;
+import static com.northwindlabs.kartikeya.recipezest.RZestContract.UserFavorites._ID;
 
 public class MyFavoriteActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
+    FoodToForkAdapter f2fAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +35,8 @@ public class MyFavoriteActivity extends AppCompatActivity {
 
         /* Set the content of the activity to use activity_menu.xml layout file */
         setContentView(R.layout.activity_my_favorite);
+
+        Toast.makeText(this, "Long Press Recipes to Remove", Toast.LENGTH_LONG).show();
 
         /* Set the toolbar as the action bar */
         Toolbar toolbar = findViewById(R.id.home_toolbar);
@@ -101,6 +117,90 @@ public class MyFavoriteActivity extends AppCompatActivity {
                     }
                 }
         );
+
+        ListView favoriteListView = findViewById(R.id.list);
+
+        // Create a new adapter that takes an empty list of recipes as input
+        f2fAdapter = new FoodToForkAdapter(this, new ArrayList<FoodToForkRecipe>());
+
+        // To access our database, we instantiate our subclass of SQLiteOpenHelper
+        // and pass the context, which is the current activity.
+        RecipeZestDBHelper mDbHelper = new RecipeZestDBHelper(this);
+
+        // Create and/or open a database to read from it
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        // Perform this raw SQL query "SELECT * FROM ingredients"
+        // to get a Cursor that contains all rows from the ingredients table.
+        Cursor mCursor = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+
+        final ArrayList<FoodToForkRecipe> favoritesArrayList = new ArrayList<>();
+        int columnIndexRecipeName = mCursor.getColumnIndex(RZestContract.UserFavorites.COLUMN_FAVORITE_RECIPE_NAME);
+        int columnIndexRecipeImage = mCursor.getColumnIndex(RZestContract.UserFavorites.COLUMN_FAVORITE_RECIPE_IMAGE);
+        int columnIndexRecipeUrl = mCursor.getColumnIndex(RZestContract.UserFavorites.COLUMN_FAVORITE_RECIPE_URL);
+        int columnIndexRecipePublisher = mCursor.getColumnIndex(RZestContract.UserFavorites.COLUMN_FAVORITE_RECIPE_PUBLISHER);
+        for (mCursor.moveToFirst(); !mCursor.isAfterLast(); mCursor.moveToNext()) {
+            // The Cursor is now set to the right position
+            FoodToForkRecipe f2fRecipe = new FoodToForkRecipe(mCursor.getString(columnIndexRecipeName), mCursor.getString(columnIndexRecipeUrl), mCursor.getString(columnIndexRecipeImage), null, mCursor.getString(columnIndexRecipePublisher));
+            favoritesArrayList.add(f2fRecipe);
+        }
+        //Invalidating the cursor, hence, releasing the resources.
+        mCursor.close();
+
+        f2fAdapter.addAll(favoritesArrayList);
+
+        // Set the adapter on the ListView
+        // so the list can be populated in the user interface
+        favoriteListView.setAdapter(f2fAdapter);
+
+        favoriteListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                FoodToForkRecipe currentRecipe = f2fAdapter.getItem(position);
+                String currentRecipeTitle = currentRecipe.getTitle();
+
+                RecipeZestDBHelper mDbHelper = new RecipeZestDBHelper(getBaseContext());
+                SQLiteDatabase dbr = mDbHelper.getReadableDatabase();
+                Cursor mCursor = dbr.rawQuery("SELECT " + _ID + " FROM " + TABLE_NAME + " where " + COLUMN_FAVORITE_RECIPE_NAME + " = \"" + currentRecipeTitle + "\"", null);
+                int minId = -1;
+                int columnIndex = mCursor.getColumnIndex(_ID);
+                for (mCursor.moveToFirst(); !mCursor.isAfterLast(); mCursor.moveToNext()) {
+                    // The Cursor is now set to the right position
+                    minId = mCursor.getInt(columnIndex);
+                    break;
+                }
+                mCursor.close();
+                if (minId != -1) {
+                    // Create and/or open a database to read from it
+                    SQLiteDatabase db = mDbHelper.getWritableDatabase();
+                    db.execSQL("delete from " + TABLE_NAME + " where " + _ID + " = " + minId);
+                }
+                //To refresh activity
+                Intent intent = getIntent();
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                finish();
+                startActivity(intent);
+                return true;
+            }
+        });
+
+        favoriteListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Find the current recipe that was clicked on
+                FoodToForkRecipe currentRecipe = f2fAdapter.getItem(position);
+
+                // Convert the String URL into a URI object (to pass into the Intent constructor)
+                Uri recipeUri = Uri.parse(currentRecipe.getF2fUrl());
+
+                // Create a new intent to view the recipe URI
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, recipeUri);
+
+                // Send the intent to launch a new activity
+                startActivity(websiteIntent);
+            }
+        });
+
     }
 
     //Open the drawer when the button is tapped
